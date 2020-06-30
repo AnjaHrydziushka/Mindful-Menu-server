78e8d36bd33b0c31547d086487c91e28bc557f5b
AnjaHrydziushka <anja.hryd@gmail.com>
2020-06-30 14:13:48 +0200

    Created first model

---
+++ models/recipe.js
@@ -0,0 +1,12 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const recipe = sequelize.define('recipe', {
+    title: DataTypes.STRING,
+    image: DataTypes.STRING,
+    description: DataTypes.STRING
+  }, {});
+  recipe.associate = function(models) {
+    // associations can be defined here
+  };
+  return recipe;
+};
