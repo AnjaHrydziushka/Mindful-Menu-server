5f132da641b66d1897eabc1669c48db9962fc41f
AnjaHrydziushka <anja.hryd@gmail.com>
2020-06-30 14:19:10 +0200

    Created a dummy data for the first model.

---
+++ seeders/20200630121448-recipes-list.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert(
+      "recipes",
+      [
+        {
+          title: "Banana pancakes",
+          image: "https://live.staticflickr.com/4813/31722526737_1c3a5518fb_b.jpg",
+          description: "Cut banana in small pieces. Whip up egg to a thick foam. Whip up egg to a thick foam. Add banana, milk and flour, mix it very good. Cook pancakes on a pan.",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        }
+      ],
+      {}
+    );
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete("recipes", null, {});
+  }
+};
