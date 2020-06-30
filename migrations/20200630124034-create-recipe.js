5695566fe765b4737ed8788862cbf9d0907cab67
AnjaHrydziushka <anja.hryd@gmail.com>
2020-06-30 14:47:20 +0200

    Reorganized models relations

--- migrations/20200630124034-create-recipe.js
+++ migrations/20200630124034-create-recipe.js
@@ -1,14 +1,23 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('tags', {
+    return queryInterface.createTable('recipes', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      name: {
+      title: {
+        type: Sequelize.STRING
+      },
+      image: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      tag: {
         type: Sequelize.STRING
       },
       createdAt: {
@@ -22,6 +31,6 @@ module.exports = {
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('tags');
+    return queryInterface.dropTable('recipes');
   }
 };
--- seeders/20200630124200-recipe-list.js
+++ seeders/20200630124200-recipe-list.js
@@ -8,7 +8,8 @@ module.exports = {
         {
           title: "Banana pancakes",
           image: "https://live.staticflickr.com/4813/31722526737_1c3a5518fb_b.jpg",
-          description: "Cut banana in small pieces. Whip up egg to a thick foam. Whip up egg to a thick foam. Add banana, milk and flour, mix it very good. Cook pancakes on a pan.",
+          description: "Cut banana in small pieces. Whip up egg to a thick foam. Whip up egg to a thick foam. Add banana, milk and flour, mix it very good. Fry pancakes on a pan.",
+          tag: "Breakfast",
           createdAt: new Date(),
           updatedAt: new Date(),
         }
