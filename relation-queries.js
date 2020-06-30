19683c5b8aaba72a610411377a23d825fb6bb441
AnjaHrydziushka <anja.hryd@gmail.com>
2020-06-30 16:42:06 +0200

    Checked more queries

--- relation-queries.js
+++ relation-queries.js
@@ -8,4 +8,29 @@ async function listsWithRecipes() {
     return recipes.map((recipe) => recipe.get({ plain: true }));
   }
   
-  listsWithRecipes().then((recipe) => console.log(recipe));
+  // listsWithRecipes().then((recipe) => console.log(recipe));
+
+  async function oneRecipe() {
+    const oneRecipe = await recipe.findByPk(1);
+    return oneRecipe.get({ plain: true });
+  }
+  
+  // oneRecipe(1).then(oneRecipe => console.log("one recipe by id", oneRecipe));
+
+  async function ingredientList() {
+    const ingredients = await ingredient.findAll();
+  
+    return ingredients.map((product) => product.get({ plain: true }));
+  }
+  
+  // ingredientList().then((product) => console.log(product));
+
+  async function ingredientsWithAmount() {
+    const ingredients = await ingredient.findAll({
+      include: { model: recipe_ingredient, attributes: ["amount"] },
+    });
+  
+    return ingredients.map((product) => product.get({ plain: true }));
+  }
+  
+  ingredientsWithAmount().then((product) => console.log(product));
