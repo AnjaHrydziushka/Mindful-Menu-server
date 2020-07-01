26f31559ed82de7dafba0c08eaefc10d6af49a6d
AnjaHrydziushka <anja.hryd@gmail.com>
2020-07-01 11:32:38 +0200

    Installed Cors, used cors middleware

--- index.js
+++ index.js
@@ -2,16 +2,15 @@ const express = require("express");
 const app = express();
 const PORT = 4000;
 const recipesRouter = require("./routers/recipes");
+const corsMiddleWare = require("cors");
 
 app.use(express.json());
+
+app.use(corsMiddleWare());
 
 app.post("/echo", (req, res) => {
     res.json(req.body);
   });
-
-// app.get("/", (req, res) => {
-//     res.json(req.body)
-// })
 
 app.use("/", recipesRouter)
 
