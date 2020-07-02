a99e046e0e0c33eafbb782f6c1b016bb70f0fb9e
AnjaHrydziushka <anja.hryd@gmail.com>
2020-07-02 10:19:59 +0200

    GET router for 1 recipe, changed router for all recipes

--- routers/recipes.js
+++ routers/recipes.js
@@ -5,16 +5,31 @@ const Recipe = require("../models").recipe;
 const Ingredient = require("../models").ingredient;
 const Quantity = require("../models").quantity;
 
-// GET list of recipes
+// GET list of all recipes
 router.get("/", async (req, res, next) => {
     try {
-        const recipes = await Recipe.findAll({ 
+        const recipes = await Recipe.findAll();
+        res.json(recipes);
+    } catch(e) {
+        next(e)
+    }
+})
+
+// GET 1 recipe by ID
+router.get("/recipes/:id", async (req, res, next) => {
+    try {
+        const id = parseInt(req.params.id)
+        const oneRecipe = await Recipe.findByPk(id, {
             include: {
                 model: Ingredient,
                 include: [Quantity]
             }
-        });
-        res.json(recipes);
+        })
+        if(!oneRecipe) {
+            res.status(404).send("Recipe not found")
+        } else {
+            res.send(oneRecipe)
+        } 
     } catch(e) {
         next(e)
     }
