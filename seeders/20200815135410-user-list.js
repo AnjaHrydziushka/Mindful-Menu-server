018069fed10948a6abb5117b98b18d6c6081d0cd
AnjaHrydziushka <anja.hryd@gmail.com>
2020-08-15 16:00:47 +0200

    Created a new seeder for User model

---
+++ seeders/20200815135410-user-list.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert(
+      "users", 
+      [
+        {
+          email: "a@a.com",
+          password: "a",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        }
+      ]
+    )
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete("users", null, {});
+  }
+};
